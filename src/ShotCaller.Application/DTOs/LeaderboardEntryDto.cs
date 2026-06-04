487dd6c3e7938f21f734786460d683bde00c75bc
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-06-04 17:34:54 +0200

    Added `LeaderboardEntryDto` to represent leaderboard entries. Introduced `ILeaderboardService` with `GetLeaderboardAsync` method. Enhanced `IPredictionService` with methods for managing predictions, including retrieval, creation, updating, and deletion.

---
+++ src/ShotCaller.Application/DTOs/LeaderboardEntryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShotCaller.Application.DTOs;
+
+public class LeaderboardEntryDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public int TotalPoints { get; set; }
+    public int PredictionCount { get; set; }
+}
+
---
+++ src/ShotCaller.Application/Interfaces/IPredictionService.cs
@@ -0,0 +1,17 @@
+﻿using ShotCaller.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShotCaller.Application.Interfaces
+{
+    public interface IPredictionService
+    {
+        Task<IEnumerable<PredictionDto>> GetAllPredictionsAsync();
+        Task<IEnumerable<PredictionDto>> GetPredictionsByMatchIdAsync(int matchId);
+        Task<IEnumerable<PredictionDto>> GetPredictionsByUserAsync(string userName);
+        Task<PredictionDto> CreatePredictionAsync(CreatePredictionDto dto);
+        Task<PredictionDto?> UpdatePredictionAsync(int id, UpdatePredictionDto dto);
+        Task<bool> DeletePredictionAsync(int id);
+    }
+}
