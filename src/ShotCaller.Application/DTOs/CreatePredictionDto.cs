4cb62d5d6fc42e36fa609b5bbf795b942b16a8c1
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-06-04 17:19:40 +0200

    Lade till klasserna `CreateMatchDto`, `CreatePredictionDto`, `PredictionDto`, `UpdateMatchResultDto` och `UpdatePredictionDto` i `ShotCaller.Application.DTOs`-namnrymden för att stödja skapande, uppdateringar och datarapportering av matcher och förutsägelser.

---
+++ src/ShotCaller.Application/DTOs/CreatePredictionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShotCaller.Application.DTOs
+{
+    public class CreatePredictionDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public int MatchId { get; set; }
+        public int PredictedHomeScore { get; set; }
+        public int PredictedAwayScore { get; set; }
+    }
+}
---
+++ src/ShotCaller.Application/Interfaces/IMatchService.cs
@@ -0,0 +1,16 @@
+﻿using ShotCaller.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShotCaller.Application.Interfaces
+{
+    public interface IMatchService
+    {
+        Task<IEnumerable<MatchDto>> GetAllMatchesAsync();
+        Task<MatchDto?> GetMatchByIdAsync(int id);
+        Task<MatchDto> CreateMatchAsync(CreateMatchDto dto);
+        Task<MatchDto?> UpdateMatchResultAsync(int id, UpdateMatchResultDto dto);
+        Task<bool> DeleteMatchAsync(int id);
+    }
+}
